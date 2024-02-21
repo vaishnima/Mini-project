589a018a9b022270f8f31fa03a6e3479281fad7d
vaishnima <pvaishnima@gmail.com>
2024-02-21 10:04:08 -0800

    homepage

--- src/components/User/HomePage/HomePage.js
+++ src/components/User/HomePage/HomePage.js
@@ -5,8 +5,8 @@ function HomePage() {
     <div className='home'><img className="home img"src="https://d1pdzcnm6xgxlz.cloudfront.net/bottoms/8905074689262-18.jpg" alt="" height="100%" width="100%"/>
     
     <h2></h2>
-    </div>
-    <div><img className='home icon'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYEjDIUJtfWV3kut0rYR9XmV7LVWB0zXgqPEYDgJ6dpIP11Gw0VjTbAxao0g&s"/> </div>
+    
+    <img className='home icon'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYEjDIUJtfWV3kut0rYR9XmV7LVWB0zXgqPEYDgJ6dpIP11Gw0VjTbAxao0g&s"/> </div>
   )
 }
 
